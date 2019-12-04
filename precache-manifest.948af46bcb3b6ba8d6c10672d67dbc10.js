beae391a29d75b6edbeca2195d4661d394279e5f
ankitgupta_1 <ankitgupta1697@gmail.com>
2019-12-04 20:39:34 +0530

    initialized project for gitpage

---
+++ precache-manifest.948af46bcb3b6ba8d6c10672d67dbc10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3872eae5418eacc54ae99993fd53b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ae7813d03587119654a",
+    "url": "/static/css/main.ff326502.chunk.css"
+  },
+  {
+    "revision": "697d6620735018e4ef72",
+    "url": "/static/js/2.cfaf7304.chunk.js"
+  },
+  {
+    "revision": "7ae7813d03587119654a",
+    "url": "/static/js/main.43cb4e82.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
